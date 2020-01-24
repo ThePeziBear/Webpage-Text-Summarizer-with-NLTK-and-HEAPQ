b8e3dec7cc013bb9ac3b9bdbebb4798196d46447
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-01-24 12:28:03 +0100

    Final Summary, Code aufgeräumt

--- Financial Feed RSS Summarizer_V6.py
+++ Financial Feed RSS Summarizer_V6.py
@@ -14,8 +14,8 @@ import feedparser
 # input: a string
 # output:a list with the words in the sentence, the words are lowered
 def sentence_tokenizer(sentence):
-    clean_1 = re.sub(r'\[[0-9]*\]', ' ', sentence.lower())
-    clean_2 = re.sub(r'[^a-zA-Z]', ' ', clean_1)
+    #clean_1 = re.sub(r'\[[0-9]*\]', ' ', sentence)
+    clean_2 = re.sub(r'[^a-zA-Z]', ' ', sentence.lower())
     clean_3 = re.sub(r'\s+', ' ', clean_2)
     clean_4 = nltk.word_tokenize(clean_3)
     return clean_4
@@ -28,19 +28,14 @@ def parse_feed(url):
     NewsFeed = feedparser.parse(url)
     return NewsFeed
 
-def clean_paragraphs(paragraphs):
-    allParagaphContent_CleanerData = re.sub(r'\[[0-9]*\]', ' ', paragraphs)
-    allParagaphContent_CleanedData = re.sub(r'\s+', ' ', allParagaphContent_CleanerData)
-    #Hier sind richtige Sätze mit Satzzeichen
-    return allParagaphContent_CleanedData
-
-def get_xpath_text(url):
+#get the text from your xpath
+def get_xpath_text(url, xpath):
     response = urlopen(url)
     htmlparser = etree.HTMLParser()
     tree = etree.parse(response, htmlparser)
 
-    expr = '//*[@id="RegularArticle-ArticleBody-5"]/div[2]'
-    text = tree.xpath(expr)
+
+    text = tree.xpath(xpath)
     textlist = ''
     if len(text) == 0 : return ''
     for line in text[0]:
@@ -48,32 +43,21 @@ def get_xpath_text(url):
         textlist += (line.text + ' ')
     return textlist
 
-# Creating Sentence Token
-def sent_tokensize(self, strings):
-    return [self.tokensize(allParagaphContent_CleanedData) for allParagaphContent in strings]
-    nltk.sent_tokensize(allParagaphContent_CleanedData)
-
 # Get Feed
 def get_feed(feedurl):
     feed = parse_feed(feedurl)
-    get_xpath_text(feed.entries[0].link)
     alltext =''
     for entry in feed.entries:
-        feedtext= get_xpath_text(entry.link)
+        feedtext= get_xpath_text(entry.link,'//*[@id="RegularArticle-ArticleBody-5"]/div[2]')
         alltext += (feedtext + ' ')
     return alltext
 
 url = 'https://www.cnbc.com/id/10000664/device/rss/rss.html'
 text = get_feed(url)
 
-
-allParagaphContent_CleanedData = re.sub(r'[^a-zA-Z]', ' ', text)
-allParagaphContent_CleanedData = re.sub(r'\s+', ' ', allParagaphContent_CleanedData)
-#allParagaphContent_CleanedData ist reiner Nudltext
-
 sentences = nltk.sent_tokenize(text)
 
-words_tokens = sentence_tokenizer(text.lower())
+words_tokens = sentence_tokenizer(text)
 
 # Get useless words in english
 stopwords = nltk.corpus.stopwords.words('english')
@@ -92,7 +76,7 @@ sentences_scores = {}
 
 sentenceAndValue = pd.DataFrame(columns={'Sentence', 'Value'})
 for sentence in sentences:
-    tokens = sentence_tokenizer(sentence.lower())
+    tokens = sentence_tokenizer(sentence)
     clean_tokens = [word for word in tokens if word not in stopwords]
     length = len(clean_tokens)
     value = 0
@@ -105,5 +89,13 @@ for sentence in sentences:
     sentenceAndValue = sentenceAndValue.append({'Sentence': sentence, 'Value': sentence_value}, ignore_index=True)
 
 summary_machineLearning = sentenceAndValue.sort_values('Value', ascending=False)
-summary_machineLearning= summary_machineLearning.drop_duplicates()
-print(summary_machineLearning['Sentence'].head(10))
+summary_machineLearning = summary_machineLearning.drop_duplicates()
+summary_machineLearning['Sentence'] = summary_machineLearning['Sentence'].replace("\"", ' ')
+
+summary_machineLearning = summary_machineLearning.head(10)
+summary_machineLearning['Sentence'] = [re.sub(r'\"\s*', ' ', clean) for clean in summary_machineLearning['Sentence'].values]
+summary = ''
+for sentence in summary_machineLearning['Sentence'].values:
+    summary += (sentence + '\n')
+
+print(summary)
